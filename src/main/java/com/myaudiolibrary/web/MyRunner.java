093ba33178898c270fcecbf5474510fc66b773a5
VanciaC <vancia.chan@ipilyon.net>
2020-11-22 15:53:28 +0100

    Set myRunner

---
+++ src/main/java/com/myaudiolibrary/web/MyRunner.java
@@ -0,0 +1,13 @@
+package com.myaudiolibrary.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
